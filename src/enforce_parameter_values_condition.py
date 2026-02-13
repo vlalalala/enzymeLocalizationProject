6953c945cc6966fdd2aa678bdffcf79bd35a4a95
uniLaptop2 <t37tum@outlook.com>
2026-02-13 16:22:11 +0100

    add first thoughts on enforcing conditions on viable parameter values

---
+++ src/enforce_parameter_values_condition.py
@@ -0,0 +1,24 @@
+# UNDER CONSTRUCTION
+import sys
+
+"""
+incompatible_enzyme_groups_list: [[A,B], [A,B,C]] # for example
+
+maximum_enzyme_concentration: # make decision on unit mol per m**3 or mol/liter
+
+
+
+"""
+# Condition 1: The total amount of enzymes must be a given number.
+
+# Condition 2: There is a maximum concentration of enzymes allowed.
+
+# Condition 3: Some sets of enzymes should not be placed within the same regions
+
+# Important: print all names of folders removed and how many are left
+
+# Make as snakemake rule. Probably best to not delete the folders...
+# Just put file in folder: .conditions_to_run_met.yaml  should have true for all conditions
+
+#if __name__ == "__main__":
+#    folder_with_parameter_ranges = sys.argv[1]
